8bbaa918cb76fba767ab39b6c01b587bc19d0fa9
andreialexc30 <andreialexx30@gmail.com>
2022-05-05 15:10:08 +0300

    Implemented ip tracking code and display on the leaflet map at the coordinates with a marker

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ const displayISP = document.getElementById('isp')
 
 // Display own user IP on page load (not working due to CORS and proxy)
 window.addEventListener('DOMContentLoaded', () => {
-    const displayed = `https://cors-anywhere.herokuapp.com/https://geo.ipify.org/api/v2/country?apiKey=${API_KEY}&ipAddress`;
+    const displayed = `https://cors-anywhere.herokuapp.com/https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress`;
+
     returnData(displayed);
 })
 
@@ -17,11 +18,12 @@ searchBtn.addEventListener('click', (e) => {
     e.preventDefault();
     const input = document.getElementById('userInput');
     let searchQuery = input.value.trim().toLowerCase();
-    let endpoint = `https://cors-anywhere.herokuapp.com/https://geo.ipify.org/api/v2/country?apiKey=${API_KEY}&ipAddress=${searchQuery}&domain=${searchQuery}`;
+    let endpoint = `https://cors-anywhere.herokuapp.com/https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${searchQuery}&domain=${searchQuery}`;
 
     returnData(endpoint)
 })
 
+// Fetching data
 function returnData(get) {
     fetch(get).then((response) => {
         if(!response.ok) {
@@ -31,20 +33,50 @@ function returnData(get) {
         return response.json();
     }).then((data) => {
         displayIP.textContent = data.ip;
-        displayLoc.textContent = `${data.location.region}, ${data.location.country}`;
-        displayTZ.textContent = data.location.timezone;
+        displayLoc.textContent = `${data.location.city}, ${data.location.region}`;
+        displayTZ.textContent = `UTC${data.location.timezone}`;
         displayISP.textContent = data.isp
+        let lat = data.location.lat;
+        let lng = data.location.lng;
+
+        switchView(lat, lng);
     }).catch((error) => console.log(error))
 }
 
 // LeafletJS map
-const map = L.map('map').setView([51.505, -0.09], 13);
+function switchView(lat, lng) {
+    // Init map
+    const map = L.map('map', {
+        center: [lat, lng],
+        zoom: 15,
+    });
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiYW5kcmVpYzMwIiwiYSI6ImNsMnJmajFueDA1azczbHBnNWNuamxoemoifQ.lytukgT8b5kLUHTMT6FM0w'
-}).addTo(map);
+    // Check if map is init -- not working as intended, fix in progress
+    const container = L.DomUtil.get('map');
+    if(container !== null) {
+        container._leaflet_id = null;
+    }
+
+    // Set map view based on coordiantes
+    map.setView(new L.LatLng(lat, lng), 15);
+
+    // Create marker icon
+    const pinIcon = L.icon({
+        iconUrl: './images/icon-location.svg',
+        iconSize: [35, 50],
+        iconAnchor: [22, 94]
+    })
+
+    // Add marker to map
+    const marker = L.marker([lat, lng], {icon: pinIcon}).addTo(map)
+
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoiYW5kcmVpYzMwIiwiYSI6ImNsMnJmajFueDA1azczbHBnNWNuamxoemoifQ.lytukgT8b5kLUHTMT6FM0w'
+    }).addTo(map);
+}
